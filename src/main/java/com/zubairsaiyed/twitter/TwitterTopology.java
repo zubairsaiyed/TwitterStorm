e203122d7f2d656f03d5df0fcd653fef116862ba
Zubair Saiyed <zubair.saiyed@gmail.com>
2016-06-15 13:50:54 -0400

    fixed storm-kafka import. building successfully

--- src/main/java/com/zubairsaiyed/twitter/TwitterTopology.java
+++ src/main/java/com/zubairsaiyed/twitter/TwitterTopology.java
@@ -1,8 +1,8 @@
 package com.zubairsaiyed.twitter;
 
-import storm.kafka.KafkaSpout;
-import storm.kafka.SpoutConfig;
-import storm.kafka.ZkHosts;
+import org.apache.storm.kafka.KafkaSpout;
+import org.apache.storm.kafka.SpoutConfig;
+import org.apache.storm.kafka.ZkHosts;
 import org.apache.storm.Config;
 import org.apache.storm.LocalCluster;
 import org.apache.storm.StormSubmitter;
@@ -27,9 +27,14 @@ public class TwitterTopology {
   private static final String REPORT_BOLT_ID = "acking-report-bolt";
 
   public static void main(String[] args) throws Exception {
-    TopologyBuilder builder = new TopologyBuilder();
 
     KafkaSpout kspout = buildKafkaSentenceSpout();
+    SplitSentenceBolt splitBolt = new SplitSentenceBolt();
+    WordCountBolt countBolt = new WordCountBolt();
+    ReportBolt reportBolt = new ReportBolt();
+
+    TopologyBuilder builder = new TopologyBuilder();
+
     builder.setSpout(SENTENCE_SPOUT_ID, kspout, 1);
     builder.setBolt(SPLIT_BOLT_ID, splitBolt).shuffleGrouping(SENTENCE_SPOUT_ID);
     builder.setBolt(COUNT_BOLT_ID, countBolt).fieldsGrouping(SPLIT_BOLT_ID, new Fields("word"));
